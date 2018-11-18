83af57d31593823fee3a9ef12b9a5a4846fba754
arisa <arisankochira10@gmail.com>
2018-11-18 10:16:43 +0900

    関数を違う形でシンプルにした

--- warikan.py
+++ warikan.py
@@ -1,14 +1,26 @@
-amount = int(input("合計おいくら？: "))
-number_of_people = int(input("人数は？: "))
+amount = 1500
+number_of_people = 3
 
 print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+amount = 2000
+number_of_people = 3
+
+print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+amount = 3647
+number_of_people = 4
+
+print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
 
 # table1 :1500 3
 # table2 : 2000 3
 # table3 : 3647 4
+def warikan(amout, number_of_people):
+    print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
 
-amount = 1500
-number_of_people = 3
 
-amount = int(input("合計おいくら？: "))
-number_of_people = int(input("人数は？: "))
+print(warikan(amount=1500, number_of_people=3))
+print(warikan(amount=2000, number_of_people=3))
+print(warikan(amount=3000, number_of_people=4))
