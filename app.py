81e240ebdfdecf230510083ae23e0b5c6d8b0876
arisa <arisankochira10@gmail.com>
2018-11-18 09:27:03 +0900

    関数の定義を学んだ

---
+++ app.py
@@ -0,0 +1,30 @@
+def hello():
+    print('hello world')
+
+
+hello()
+
+
+def sey_hello(name):
+    print(f"Hi {name}")
+
+
+sey_hello("BOb")
+
+
+def double(number):
+    return 2 * number
+
+
+reesult_1 = double(3)
+print(reesult_1)
+
+
+#
+def str_combine(str1, str2):
+    return f"{str1}{str2}"
+
+
+result = str_combine('Kazuma', 'Takahashi')
+
+print(result)
