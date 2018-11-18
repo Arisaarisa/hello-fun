95061b284f3b5de95ddabfa331132e32b3aa71de
arisa <arisankochira10@gmail.com>
2018-11-18 10:44:26 +0900

    さらにシンプルに関数を作るリファクタで関数の名前を変える

---
+++ app2.py
@@ -0,0 +1,4 @@
+# 割り勘計算したい7000円6にん
+from warikan2 import calculate_warikan
+
+print(calculate_warikan(amount=7000, number_of_people=6))
--- warikan.py
+++ warikan.py
@@ -19,8 +19,3 @@ print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number
 # table3 : 3647 4
 def warikan(amout, number_of_people):
     print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
-
-
-print(warikan(amount=1500, number_of_people=3))
-print(warikan(amount=2000, number_of_people=3))
-print(warikan(amount=3000, number_of_people=4))
