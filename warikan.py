fa374b380526c95977b7bcadc67fa215e378f7f4
arisa <arisankochira10@gmail.com>
2018-11-18 09:42:02 +0900

    3テーブル分の割り勘全然時間なかった

---
+++ warikan.py
@@ -0,0 +1,14 @@
+amount = int(input("合計おいくら？: "))
+number_of_people = int(input("人数は？: "))
+
+print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+# table1 :1500 3
+# table2 : 2000 3
+# table3 : 3647 4
+
+amount = 1500
+number_of_people = 3
+
+amount = int(input("合計おいくら？: "))
+number_of_people = int(input("人数は？: "))
